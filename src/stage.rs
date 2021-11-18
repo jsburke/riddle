399a1b68f513bf030266860b4cf0066e78876e19
jsburke <jsburke@bu.edu>
2021-11-18 16:26:59 -0500

    fix matrix error

--- src/stage.rs
+++ src/stage.rs
@@ -3,9 +3,10 @@
 /// the internal representation of the consumed source
 /// file that may be actively modified during evalutation
 pub struct Stage {
-  /// The elements used in the CircStack
+  /// dimenstions of the stage
   num_rows: usize,
   num_cols: usize,
-  tiles:    [[u8; num_rows]; num_cols],
+  /// characters from the file, padded with spaces
+  tiles:    Vec<Vec<u8>>
 }
 
