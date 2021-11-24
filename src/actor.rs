b7164d92a7f76225f8ede30202efc113390aed9f
jsburke <jsburke@bu.edu>
2021-11-24 13:10:21 -0500

    some code movement for coordinates and such

--- src/actor.rs
+++ src/actor.rs
@@ -1,7 +1,32 @@
+use meta::*;
+
+/// The direction enum is used to both indicate how
+/// actors move and face in Riddle
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+/// Coords describes the location of actors in a given
+/// stage in Riddle
+type Coord = (StageUnit, StageUnit);
+
 /// Actors in Riddle are elements that operate within
 /// a given stage with one being player controllable,
 /// and others being automata in Riddle that can alter
 /// execution or play
 pub struct Actor {
-  coords: Coords,
+  /// location in the current Stage
+  coords:    Coord,
+
+  /// which way is the actor facing
+  dir:       Direction,
+
+
+  is_player: bool,
+
+  /// if player, current stacks
+  stacks:    Vec<*Circstack<StackType>>,
 }
---
+++ src/meta.rs
@@ -0,0 +1,7 @@
+/// StageUnit is the datatype used to
+/// locate things in the cartesian plane
+type StageUnit = usize;
+
+/// StackType is the datatype used in
+/// the Riddle Stack
+type StackType = i16;
